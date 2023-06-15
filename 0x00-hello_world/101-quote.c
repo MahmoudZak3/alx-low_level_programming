bd9d39cdf08e3e6a333cd9013be25f8d11101598
zak <modzak9@gmail.com>
2023-06-15 19:59:06 +0300

    Zak 0x00 hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
-#include<stdio.h>
+#include<unistd.h>
 /**
  * main - Entry point
  * Description: 101-quote.c
  *
- * Return: Always 1 (Not Success)
+ * Return: Always 1 (N Success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, str, 59);
 	return (1);
 }
