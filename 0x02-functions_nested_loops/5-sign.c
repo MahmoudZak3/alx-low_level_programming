f0dc3f879b2a0c9081f90e7c96a49fc9c0c3d8a5
MahmoudZak3 <modzak99@yahoo.com>
2023-06-20 23:31:39 +0300

    zak com

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,7 +14,7 @@ int print_sign(int n)
 		return (1);
 	}
 	else if (n == 0)
-	{	
+	{
 		_putchar(48);
 		return (0);
 	}
@@ -22,5 +22,5 @@ int print_sign(int n)
 	{
 		_putchar(45);
 		return (-1);
-	}	
+	}
 }
