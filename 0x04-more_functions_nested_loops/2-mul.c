06f15055846343e821abf4c127e960395180a35c
Mahmoud Zakaria <modzak9@gmail.com>
2023-06-22 09:46:07 -0700

    za

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply tow int degets
+ * @a: first deget
+ * @b: second deget
+ * Return: 1 if succes or 0 else if not succes addition of the multiplicattion
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
