f9b67d02861d59acff8a9502044ff673d8d68962
Mahmoud Zakaria <modzak9@gmail.com>
2023-07-17 09:39:38 -0700

    zaaaaaaaaaaaaaak

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+zaaaaaak read
