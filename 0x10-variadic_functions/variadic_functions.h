5150069cc5fe71bb587e296d842930e2a2fe2a45
MahmoudZak3 <88795478+MahmoudZak3@users.noreply.github.com>
2023-07-21 00:33:54 +0300

    zaakcom

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,18 +3,18 @@
 
 /*
  * File: variadic_functions.h
- * Auth: Onyiana
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0F-variadic_functions directory.
+ * Auth: zaaak
+ * Desc: Header file containing prototypes to functions
+ *       for 0x0F-variadic_functions directory.
  */
 
 #include <stdarg.h>
 
 /**
- * struct printer - A new struct type defining a printer.
- * @symbol: A symbol representing a data type.
- * @print: A function pointer to a function that prints
- *    a data type corresponding to symbol.
+ * struct printer -  a printer struct type defining .
+ * @symbol: A symbol  a data type representing.
+ * @print:  pointer to a function that prints corresponding
+ *    a data type  to symbol.
  */
 typedef struct printer
 {
