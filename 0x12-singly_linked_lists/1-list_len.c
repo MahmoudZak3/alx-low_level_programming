65d826a7ee9a55b4b0072e4fbb42d67e02d081f3
Mahmoud Zakaria <modzak9@gmail.com>
2023-07-27 17:03:12 -0700

    zak com

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,8 +5,6 @@
  * list_len - Finds returns the number of elements in
  *            a linked list_t list.
  * @h: The  list_t list linked list_t list.
- * @elements: a counter var for size_t (nodes)
- *
  * Return:  elements in h.
  */
 size_t list_len(const list_t *h)
