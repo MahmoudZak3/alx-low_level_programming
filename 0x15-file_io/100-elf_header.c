18f3a5e174e5641ddd2021d7d9ea09267070a125
MahmoudZak3 <88795478+MahmoudZak3@users.noreply.github.com>
2023-08-07 20:57:01 +0300

    zak

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,10 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
@@ -18,10 +19,9 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
- * check_elf - Checks if a file is an ELF file.
+ * check_elf -func Checks if a file is an ELF file.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
- *
- * Description: If the file is not an ELF file - exit code 98.
+ * Description:  exit code 98 - If the file is not an ELF file.
  */
 void check_elf(unsigned char *e_ident)
 {
@@ -41,9 +41,8 @@ void check_elf(unsigned char *e_ident)
 }
 
 /**
- * print_magic - Prints the magic numbers of an ELF header.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
- *
+ * print_magic -Prints the class of an ELF header.
+ * @e_ident: the pointer to an array containing  magic numbers.
  * Description: Magic numbers are separated by spaces.
  */
 void print_magic(unsigned char *e_ident)
@@ -64,8 +63,8 @@ void print_magic(unsigned char *e_ident)
 }
 
 /**
- * print_class - Prints the class of an ELF header.
- * @e_ident: A pointer to an array containing the ELF class.
+ * print_class - header Prints the magic numbers of an ELF.
+ * @e_ident:Magic numbers are separated by spaces.
  */
 void print_class(unsigned char *e_ident)
 {
@@ -246,7 +245,6 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 /**
  * close_elf - Closes an ELF file.
  * @elf: The file descriptor of the ELF file.
- *
  * Description: If the file cannot be closed - exit code 98.
  */
 void close_elf(int elf)
@@ -260,13 +258,10 @@ void close_elf(int elf)
 }
 
 /**
- * main - Displays the information contained in the
- *        ELF header at the start of an ELF file.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
+ * main - Displays the information contained in the ELF header.
+ * @argc: An array of pointers to the arguments.
+ * @argv:  The number of arguments supplied to the program.
  * Return: 0 on success.
- *
  * Description: If the file is not an ELF File or
  *              the function fails - exit code 98.
  */
